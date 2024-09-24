269f68b6af7dadda9b8d8ff9cb29da8a6be7b77c
devesh1808 <devesh1808@gmail.com>
2024-09-25 00:11:56 +0530

    naming conventions examples

---
+++ naming convention/index.js
@@ -0,0 +1,16 @@
+
+var myName = "Devesh";
+
+var yourName = "Hitesh";
+
+var myvar = "abc";
+
+var my123 = 123;
+
+var myname = "Dev";
+
+var abc123$_ = 12;
+
+var my_name = "D";
+
+// Note : prefer to use camelCase convention - eg. myName
