aaf1920dc64e28fc35210b3317314240e444443f
devesh1808 <devesh1808@gmail.com>
2024-09-24 21:12:47 +0530

    data types

---
+++ data types/index.js
@@ -0,0 +1,9 @@
+console.log(typeof(23));  //it will print the output in the console
+
+document.write(typeof(23));  //it will print the output in the browser
+
+document.write(typeof("Devesh"));
+
+document.write(typeof(true));
+
+document.write(typeof(23) + " " + typeof("devesh") + " " + typeof(true));
