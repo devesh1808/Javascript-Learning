e0719283709560e8a286061f3f91dac34c7335d7
devesh1808 <devesh1808@gmail.com>
2024-09-28 16:52:12 +0530

    basics of arrays

---
+++ arrays/index.js
@@ -0,0 +1,14 @@
+
+/* Arrays */
+
+var guestList = ["Devesh", "Sanjay", "Yogesh"];
+
+document.write(guestList);
+
+document.write(guestList.length);
+
+document.write(guestList[2]);
+
+document.write(guestList.includes("Sachin"));    // include() method checks whether the value is present in array or not, if present, returns true, else false
+
+
