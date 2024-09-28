762185d3a5ed53024cf22debb4eb091b0d03dd21
devesh1808 <devesh1808@gmail.com>
2024-09-28 14:01:55 +0530

    random function

---
+++ random number/index.js
@@ -0,0 +1,11 @@
+
+var n = Math.random();  // range (0 to 0.9999(upto 16 decimal places))
+document.write(n);
+
+
+/* Dice Value Generator */
+
+var num = Math.random();
+num *= 6; 
+num = Math.floor(num) + 1; 
+document.write(num);
