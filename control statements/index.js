421159533aeb2c86d0cedd60b19d225678efc68a
devesh1808 <devesh1808@gmail.com>
2024-09-28 14:19:18 +0530

    if else-if else ladder

--- control statements/index.js
+++ control statements/index.js
@@ -1,9 +1,23 @@
 /* if-else */
 
-var age = 27;
-if(age>=18){
-    document.write("you are an adult");
+// var age = 27;
+// if(age>=18){
+//     document.write("you are an adult");
+// }
+// else{
+//     document.write("you are a kid");
+// }
+
+
+/* if else-if else */
+
+var grade = 'C';
+if(grade === 'A'){
+    document.write("Excellent");
+}
+else if(grade === 'B'){
+    document.write("Good");
 }
 else{
-    document.write("you are a kid");
+    document.write("Could be better");
 }
