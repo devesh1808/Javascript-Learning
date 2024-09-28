ba6d9cfe7a6d72a51f7b549f488e195dd45fab03
devesh1808 <devesh1808@gmail.com>
2024-09-28 17:11:24 +0530

    push and pop methods in arrays

--- arrays/index.js
+++ arrays/index.js
@@ -1,14 +1,28 @@
 
 /* Arrays */
 
-var guestList = ["Devesh", "Sanjay", "Yogesh"];
+// var guestList = ["Devesh", "Sanjay", "Yogesh"];
 
-document.write(guestList);
+// document.write(guestList);
 
-document.write(guestList.length);
+// document.write(guestList.length);
 
-document.write(guestList[2]);
+// document.write(guestList[2]);
 
-document.write(guestList.includes("Sachin"));    // include() method checks whether the value is present in array or not, if present, returns true, else false
+// document.write(guestList.includes("Sachin"));    // include() method checks whether the value is present in array or not, if present, returns true, else false
 
+
+
+/* Push and Pop in array */
+
+var output = [];  //empty array
+
+output.push(1);
+output.push(2);
+output.push(3);
+output.push(4);
+
+output.pop();
+
+document.write(output);
 
