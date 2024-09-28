9386e450efededcda30bbc39f454cf1f3d738d58
devesh1808 <devesh1808@gmail.com>
2024-09-28 23:44:37 +0530

    for loop

--- flow control - loops/index.js
+++ flow control - loops/index.js
@@ -1,7 +1,15 @@
 /* While Loop */
 
-var i = 1;
-while(i<6){
-    document.write(i);
-    i++;
+// var i = 1;
+// while(i<6){
+//     document.write(i);
+//     i++;
+// }
+
+
+
+/* For Loop */
+
+for(var i=1; i<=5; i++){
+    document.write(i + " ");
 }
