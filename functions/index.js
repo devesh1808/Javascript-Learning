75f5f0953d4c21f60b323922d62794bc3672394f
devesh1808 <devesh1808@gmail.com>
2024-09-28 12:56:27 +0530

    bmi calculator code

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 
 /* Function */
 
-function add(a,b){
-    return a+b;
+// function add(a,b){
+//     return a+b;
+// }
+
+// var c = add(3,6);
+// document.write(c);
+
+
+/* BMI Calculator Coding Challange */
+
+function bmiCalculator(weight, height){
+    return Math.floor(weight/(height*height));
 }
 
-var c = add(3,6);
-document.write(c);
+var bmi = bmiCalculator(65, 1.8);
+document.write(bmi);
