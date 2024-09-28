5d05ee81ffa6f9076e1649df5a472fb5a5a133e3
devesh1808 <devesh1808@gmail.com>
2024-09-28 16:39:58 +0530

    leap year code

--- control statements/index.js
+++ control statements/index.js
@@ -8,16 +8,38 @@
 //     document.write("you are a kid");
 // }
 
-
 /* if else-if else */
 
-var grade = 'C';
-if(grade === 'A'){
-    document.write("Excellent");
+// var grade = 'C';
+// if(grade === 'A'){
+//     document.write("Excellent");
+// }
+// else if(grade === 'B'){
+//     document.write("Good");
+// }
+// else{
+//     document.write("Could be better");
+// }
+
+
+
+/* Program to check whether a year is leap year or not */
+
+function isLeap(year) {
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        return "Leap year.";
+      } else {
+        return "Not a leap year.";
+      }
+    } else {
+      return "Leap year.";
+    }
+  } else {
+    return "Not leap year.";
+  }
 }
-else if(grade === 'B'){
-    document.write("Good");
-}
-else{
-    document.write("Could be better");
-}
+
+var result = isLeap(2100);
+document.write(result);
