b44f1df2eb2652403d4594cef8967df52ff3a008
devesh1808 <devesh1808@gmail.com>
2024-09-29 18:25:56 +0530

    basics of DOM and accessing properties and methods using DOM

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,13 @@
-document.querySelector("h1").innerHTML = "Good Bye";
+document.write(document.firstElementChild.firstElementChild.firstElementChild);
+
+
+// Targetting the heading element inside body using DOM tree structure
+
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+heading.innerHTML = "Good, Bye";
+heading.style.color = "blue";
+
+
+// Targetting the checkbox in HTML using DOM
+
+document.querySelector("input").click();
