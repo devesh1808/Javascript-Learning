f8d92ecb50d137d8bdc7e774caa9581618223e7d
devesh1808 <devesh1808@gmail.com>
2024-09-29 19:34:19 +0530

    selecting html elements using js

--- DOM/index.js
+++ DOM/index.js
@@ -1,23 +1,69 @@
-document.write(document.firstElementChild.firstElementChild.firstElementChild);
+// document.write(document.firstElementChild.firstElementChild.firstElementChild);
 
 
-// Targetting the heading element inside body using DOM tree structure
+// // Targetting the heading element inside body using DOM tree structure
 
-var heading = document.firstElementChild.lastElementChild.firstElementChild;
-heading.innerHTML = "Good, Bye";
-heading.style.color = "blue";
-
-
-// Targetting the checkbox in HTML using DOM
-
-document.querySelector("input").click();
+// var heading = document.firstElementChild.lastElementChild.firstElementChild;
+// heading.innerHTML = "Good, Bye";
+// heading.style.color = "blue";
 
 
+// // Targetting the checkbox in HTML using DOM
 
-/* DOM challange - Changing the text of 3rd list item */
+// document.querySelector("input").click();
 
-document.querySelector("ul").lastElementChild.innerHTML = "Devesh has changed it";
 
-// OR
 
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Devesh has changed it again";
+// /* DOM challange - Changing the text of 3rd list item */
+
+// document.querySelector("ul").lastElementChild.innerHTML = "Devesh has changed it";
+
+// // OR
+
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Devesh has changed it again";
+
+
+
+
+/* Different ways for selecting HTML elements */
+
+// 1. querySelector()
+
+//2. getElementsByTagName()
+
+document.getElementsByTagName("li")[2].style.color = "lime";
+
+// 3. getElementsByClassName()
+
+document.getElementsByClassName("btn")[0].style.color = "blue";
+
+// 4. getElementById()
+
+document.getElementById("title").style.color = "purple";
+
+document.getElementById("list").style.color = "red";
+
+document.getElementById("title").innerHTML = "Time to Say GoodBye";
+
+
+// Note : you can fetch an element from querySelector() by mentioning the element name, class name or id name
+
+document.querySelector("h1").innerHTML = "Changed title 1";
+
+document.querySelector("#title").innerHTML = "Changed title 2";
+
+document.querySelector(".btn").style.color = "orange";
+
+
+// Combining elements or combining selectors in querySelector()
+
+document.querySelector("li a").style.color = "green";
+
+document.querySelector("li.item").style.color = "red";  // will target the first list item
+
+document.querySelector("#list a").style.color = "orange";
+
+
+// Selecting all matching selectors using querySelectorAll()
+
+document.querySelectorAll("#list .item").style.color = "red";
