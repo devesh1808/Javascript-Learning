9bfca61f775744cf38c9e9df4db2dd109ff95a82
devesh1808 <devesh1808@gmail.com>
2024-10-04 09:55:37 +0530

    adding tom-1.mp3 audio to all buttons in index.js

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -7,7 +7,7 @@
 
 
 
-// created event listener using anonymous function
+/* created event listener using anonymous function */
 
 // document.querySelector("button").addEventListener("click", function (){
 //     alert("I got clicked");
@@ -15,35 +15,50 @@
 
 
 
-// adding event listener to all buttons
+/* adding event listener to all buttons */
+
+// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// for(var i=0; i<numberOfDrumButtons; i++){
+//     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+//         alert("I got clicked");
+//     });
+// }
+
+
+
+
+/* Higher Order Function (HOF) and passing functions as argument */
+
+// HOF are functions that can take other functions as inputs
+
+// function add(num1, num2){
+//     return num1+num2;
+// }
+
+// function multiply(num1, num2){
+//     return num1*num2;
+// }
+
+// function calculator(num1, num2, operator){
+//     return operator(num1, num2);
+// }
+
+// console.log(calculator(2,6,add));
+
+// console.log(calculator(2,6,multiply));
+
+
+
+
+
+/* Playing Sound(Audio) in Js */
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for(var i=0; i<numberOfDrumButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert("I got clicked");
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
 }
-
-
-
-
-// Higher Order Function (HOF) and passing functions as argument
-
-// HOF are functions that can take other functions as inputs
-
-function add(num1, num2){
-    return num1+num2;
-}
-
-function multiply(num1, num2){
-    return num1*num2;
-}
-
-function calculator(num1, num2, operator){
-    return operator(num1, num2);
-}
-
-console.log(calculator(2,6,add));
-
-console.log(calculator(2,6,multiply));
